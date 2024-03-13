82cf07a9b2b345e5a87acf069cb8f5460f2a37cc
jason-t-pho <jtanho02@gmail.com>
2024-03-12 22:46:56 -0700

    added new design changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <div className="home-page">
       <h1>Welcome to My App!</h1>
       <p>This is the homepage of my app.</p>
       {accessToken && <p>Spotify Access Token: {accessToken}</p>}
