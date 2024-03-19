581624cea3f9106d4f8df65e34f1edde5d4d2111
jason-t-pho <jtanho02@gmail.com>
2024-03-18 19:42:20 -0700

    create playlist page stuff

--- src/pages/CreatePlaylist.jsx
+++ src/pages/CreatePlaylist.jsx
@@ -2,6 +2,16 @@ export default function Search() {
     return (
         <div className="createplaylist-page">
             <h1>Create Playlist</h1>
+            <div className="create-playlist-container">
+                <a
+                className="create-playlist-home-button josefin-sans-small-text-button"
+                href="/create-playlist"
+                >
+                    <div className="create-playlist-home-button-container">
+                Create Playlist
+                </div>
+                </a>
+            </div>
         </div>
     )
 }
