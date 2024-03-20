99a320db13a6b614cd9fa55568b122da8481a924
jason-t-pho <jtanho02@gmail.com>
2024-03-19 18:30:23 -0700

    Fixed No Hook Call Error

--- src/pages/CreatePlaylist.jsx
+++ src/pages/CreatePlaylist.jsx
@@ -3,6 +3,7 @@ import { addGenre, toggleGenre } from "../genreSlice";
 import { getSelectedGenres } from "../genreSlice";
 import { useDispatch, useSelector } from 'react-redux'
 import styled from '@emotion/styled'
+import { useEffect, useState } from "react";
 
 
 const GenreContainer = styled.div`
@@ -39,15 +40,29 @@ export function GenreItem(props) {
     )
 }
 
-export function CreatePlaylist(allGenresChecked) {
+function CreatePlaylist(props) {
+    const allGenresChecked = props
+    // perform playlist creation here
+    const token = localStorage.getItem("accessToken");
+    console.log(allGenresChecked)
+
+
+    // useEffect(() => {
+    //     // fetch categories based on allGenresChecked array
+    //     fetch("https://api.spotify.com/v1/browse/categories")
+    // }, []);
+
     // console.log(allGenresChecked)
+
 }
 
 
 export default function Search() {
     const dispatch = useDispatch()
-
     const allSliceGenres = useSelector((state) => state.genres)
+    const [selectedGenres, setSelectedGenres] = useState([]);
+
+
 
     return (
         <div className="createplaylist-page">
@@ -59,9 +74,11 @@ export default function Search() {
                 </div>
 
                 <button onClick={() => {
-                    const selectedGenres = allSliceGenres.genres.filter((g) => g.checked === true)
-                    console.log(selectedGenres)
+                    const currSelectedGenres = allSliceGenres.genres.filter((g) => g.checked === true)
+                    setSelectedGenres(currSelectedGenres)
                     }}>Save Playlist!</button>
+
+                {selectedGenres && <CreatePlaylist allGenresChecked={selectedGenres}/>}
 {/* 
                 <a
                 className="create-playlist-home-button josefin-sans-small-text-button"
