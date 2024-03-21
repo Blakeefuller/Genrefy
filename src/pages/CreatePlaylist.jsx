3c75119eea6fe99b266be4547800eaf6a2e5b2ec
jason-t-pho <jtanho02@gmail.com>
2024-03-20 23:01:47 -0700

    Grabbed all track ID's based on api call

--- src/pages/CreatePlaylist.jsx
+++ src/pages/CreatePlaylist.jsx
@@ -14,23 +14,25 @@ const GenreContainer = styled.div`
         margin: 0;
     }
     p {
-        color: ${props => props.isChecked && 'black'};
         color: black;
+        color: ${props => props.isChecked && 'red'};
         margin: 0;
         padding-left: 15px;
+        padding-top: 15px;
     }
 `
 
 export function GenreItem(props) {
     const dispatch = useDispatch()
-
     const { genre, checked } = props
+
+    let count = 1;
 
     return (
         <>
         <GenreContainer checked={checked}>
-        <div className="genre-item">
-            <button className="genre-checkbox" onClick={() => {
+        <div className="genre-item josefin-sans-small-text">
+            <button className="genre-checkbox"onClick={() => {
                 dispatch(toggleGenre(genre))
                 }}>Add</button>
             <p>{genre}</p>
@@ -44,15 +46,82 @@ function CreatePlaylist(props) {
     const allGenresChecked = props
     // perform playlist creation here
     const token = localStorage.getItem("accessToken");
-    console.log(allGenresChecked)
+    // grab all genres and set into array
+    const [genreStringArr, setGenreStringArr] = useState(allGenresChecked.allGenresChecked.map(item => item.genre));
+
+    // testing 
+    const testGenre = allGenresChecked.allGenresChecked[0].genre
+    console.log(allGenresChecked.allGenresChecked[0].genre)
 
 
-    // useEffect(() => {
-    //     // fetch categories based on allGenresChecked array
-    //     fetch("https://api.spotify.com/v1/browse/categories")
-    // }, []);
+    const [currPlaylistId, setCurrPlaylistId] = useState("");
+    const [randomTracks, setRandomTracks] = useState([]); // array or all track id's grabbed from each genre playlist
 
-    // console.log(allGenresChecked)
+    console.log(genreStringArr)
+
+    useEffect(() =>  {
+        // run fetching category per however many genres are selected
+        for (let i = 0; i < genreStringArr.length; i++) {
+            fetchCategory(genreStringArr[i]);
+        }
+        // new api call to create playlists based on all generated track ID's
+    }, []);
+
+    
+    const fetchCategory = async (genreAtIndex) => {
+        try {
+            const response = await fetch(`https://api.spotify.com/v1/browse/categories/${genreAtIndex}/playlists`, {
+                headers: {
+                    Authorization: "Bearer " + token,
+                }
+            });
+            const data = await response.json();
+            const playlistId = data.playlists.items[0].id; // HARD INPUT: First playlist grabbed from found genre
+            setCurrPlaylistId(playlistId);
+
+            console.log("== playlist id of first playlist grabbed from genres: ", playlistId);
+            
+            // Fetch playlist tracks based on retrieved playlist ID from genre
+            fetchPlaylistTrack(playlistId);
+        } catch (error) {
+            console.error("Error fetching category:", error);
+        }
+    };
+    
+    const fetchPlaylistTrack = async (playlistId) => {
+        try {
+            const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+                headers: {
+                    Authorization: "Bearer " + token,
+                }
+            });
+            const data = await response.json();
+
+            console.log("== playlist tracks amount", data.total);
+            console.log("== playlist sample track uri", data.items[0].track.uri);
+    
+            //array holding random tracks from playlist
+            const currRandomTracks = [];
+            const totalTracks = data.total;
+            // hard input 5 random tracks to be added per every genre
+            for (let i = 0; i < 5; i++) {
+                const randomIndex = Math.floor(Math.random() * totalTracks);
+                currRandomTracks.push(data.items[randomIndex].track.uri);
+            }
+
+            // set random tracks from playlist into array of all songs.
+            setRandomTracks(prevRandomTracks => [...prevRandomTracks, ...currRandomTracks]);
+
+        } catch (error) {
+            console.error("Error fetching playlist tracks:", error);
+        }
+    };
+
+    return (
+        <>
+        <h1>Created playlist! View your playlist!</h1>
+        </>
+    )
 
 }
 
@@ -60,7 +129,7 @@ function CreatePlaylist(props) {
 export default function Search() {
     const dispatch = useDispatch()
     const allSliceGenres = useSelector((state) => state.genres)
-    const [selectedGenres, setSelectedGenres] = useState([]);
+    const [selectedGenres, setSelectedGenres] = useState(null);
 
 
 
@@ -73,7 +142,7 @@ export default function Search() {
                         (<GenreItem key={index} genre={item.genre} checked={item.checked} />))}
                 </div>
 
-                <button onClick={() => {
+                <button className="save-playlist-button josefin-sans-small-text" onClick={() => {
                     const currSelectedGenres = allSliceGenres.genres.filter((g) => g.checked === true)
                     setSelectedGenres(currSelectedGenres)
                     }}>Save Playlist!</button>
